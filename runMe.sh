1fe6a80c0fda736012e1eda1e9afd24ed45c94cf
Alain Becam <abecam@gmail.com>
2026-08-06 12:13:48 +0200

    Ansible inventory for localhost

--- runMe.sh
+++ runMe.sh
@@ -12,27 +12,32 @@ mkdir docker
 cd docker
 wget https://raw.githubusercontent.com/seek4science/seek/seek-1.18/docker/db.env
 
-#mkdir solr
-#cd solr
-#mkdir seek
-#cd seek
-#mkdir conf
-#cd conf
-#wget https://raw.githubusercontent.com/seek4science/seek/refs/heads/seek-1.18/solr/seek/conf/solrconfig.xml
+mkdir solr
+cd solr
+mkdir seek
+cd seek
+mkdir conf
+cd conf
+wget https://raw.githubusercontent.com/seek4science/seek/refs/heads/seek-1.18/solr/seek/conf/solrconfig.xml
 
 cd ..
 echo "Starting SEEK containers"
 docker compose up -d
 
 echo "Waiting for SEEK to be ready"
+# won't work as seek is not the name of the container
 until docker logs seek | grep -q "Listening on"; do
   sleep 5
 done
 echo "SEEK is ready"
 
+# Ansible needs to be installed first: sudo apt install ansible
 echo "Creating initial admin user"
 wget https://raw.githubusercontent.com/abecam/prepare-seek4science/main/CreateInitialuser.yml
 ansible-playbook CreateInitialuser.yml
+ansible-playbook -i inventory.yml CreateInitialuser.yml \
+  --ask-vault-pass \
+  -e seek_admin_password='ChangeMe123!'
 
 echo "Fetching and pushing data from investigation"
 wget https://raw.githubusercontent.com/abecam/prepare-seek4science/main/FetchPushFromInvestigation.py
