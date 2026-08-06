0ecf0590384c09ba649689f4c8d6df6d3e1b90e1
Alain Becam <abecam@gmail.com>
2026-08-06 09:27:00 +0200

    Tmp fix to solr folder issue

--- runMe.sh
+++ runMe.sh
@@ -6,10 +6,19 @@ docker volume create --name=seek-solr-data
 docker volume create --name=seek-cache
 
 echo "Downloading docker-compose.yml and db.env"
-wget https://raw.githubusercontent.com/seek4science/seek/seek-1.18/docker-compose.yml
+#wget https://raw.githubusercontent.com/seek4science/seek/seek-1.18/docker-compose.yml
+wget https://raw.githubusercontent.com/abecam/prepare-seek4science/main/TestSeekDockerCompose/docker-compose.yml
 mkdir docker
 cd docker
 wget https://raw.githubusercontent.com/seek4science/seek/seek-1.18/docker/db.env
+
+#mkdir solr
+#cd solr
+#mkdir seek
+#cd seek
+#mkdir conf
+#cd conf
+#wget https://raw.githubusercontent.com/seek4science/seek/refs/heads/seek-1.18/solr/seek/conf/solrconfig.xml
 
 cd ..
 echo "Starting SEEK containers"
